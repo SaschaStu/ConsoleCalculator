5afd0900d90bbfec3c64e65be2869f15575e45d6
Sascha Sturzenegger <sascha@sturzenegger-wyss.ch>
2021-05-19 11:39:45 +0200

    Implemented the completement of functionality of function summe

--- src/main/java/ch/bbw/sst/App.java
+++ src/main/java/ch/bbw/sst/App.java
@@ -1,13 +1,25 @@
 package ch.bbw.sst;
 
 /**
- * Hello world!
+ * Console Calculator App
+ * @author Sascha Sturzenegger
+ * @version 1.0.0
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Calculator calculator = new Calculator();
+        int valueA = 0;
+        int valueB = 0;
+
+        System.out.println("Console Calculator");
+        System.out.println("******************");
+        System.out.println();
+
+        valueA = 10;
+        valueB = 20;
+        System.out.println("Summe "+valueA+"+"+valueB+"="+calculator.summe(valueA,valueB));
     }
 }
