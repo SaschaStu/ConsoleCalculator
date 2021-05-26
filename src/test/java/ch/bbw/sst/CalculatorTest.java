9ea4175624740f3cede250b2065fc38e1cfccd97
Sascha Sturzenegger <sascha@sturzenegger-wyss.ch>
2021-05-26 11:36:03 +0200

    @Before added to CalTest

--- src/test/java/ch/bbw/sst/CalculatorTest.java
+++ src/test/java/ch/bbw/sst/CalculatorTest.java
@@ -1,5 +1,6 @@
 package ch.bbw.sst;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,38 +8,37 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 
     Calculator testee;
+
+    @Before
+    public void setUP(){
+        testee = new Calculator();
+    }
     @Test
     public void testiSumme2PosOK() {
-        testee = new Calculator();
         assertTrue(testee.summe(10,25) == 35);
     }
     @Test
     public void testiSumme2NegOK(){
-        testee = new Calculator();
         assertTrue(testee.summe(-1,-2) == -3);
     }
 
     @Test
     public void testiSumme1Neg1PosOK(){
-        testee = new Calculator();
         assertTrue(testee.summe(-1,3) == 2);
     }
 
     @Test
     public void testiSubtraktion2PosOK() {
-        testee = new Calculator();
         assertTrue(testee.subtraktion(25,10)==15);
     }
 
     @Test
     public void testiSubtraktion2NegOK(){
-        testee = new Calculator();
         assertTrue(testee.subtraktion(-1,-2) == 1);
     }
 
     @Test
     public void testiSubtraktion1Neg1PosOK(){
-        testee = new Calculator();
         assertTrue(testee.subtraktion(-3,5) == -8);
     }
 }
