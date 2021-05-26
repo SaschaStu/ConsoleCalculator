4ce56b46128689ac85a38961087780dd47d01801
Sascha Sturzenegger <sascha@sturzenegger-wyss.ch>
2021-05-26 11:28:26 +0200

    4 new Tests with nice fresh names

--- src/test/java/ch/bbw/sst/CalculatorTest.java
+++ src/test/java/ch/bbw/sst/CalculatorTest.java
@@ -8,14 +8,37 @@ public class CalculatorTest {
 
     Calculator testee;
     @Test
-    public void summe() {
+    public void testiSumme2PosOK() {
         testee = new Calculator();
         assertTrue(testee.summe(10,25) == 35);
     }
+    @Test
+    public void testiSumme2NegOK(){
+        testee = new Calculator();
+        assertTrue(testee.summe(-1,-2) == -3);
+    }
 
     @Test
-    public void subtraktion() {
+    public void testiSumme1Neg1PosOK(){
+        testee = new Calculator();
+        assertTrue(testee.summe(-1,3) == 2);
+    }
+
+    @Test
+    public void testiSubtraktion2PosOK() {
         testee = new Calculator();
         assertTrue(testee.subtraktion(25,10)==15);
+    }
+
+    @Test
+    public void testiSubtraktion2NegOK(){
+        testee = new Calculator();
+        assertTrue(testee.subtraktion(-1,-2) == 1);
+    }
+
+    @Test
+    public void testiSubtraktion1Neg1PosOK(){
+        testee = new Calculator();
+        assertTrue(testee.subtraktion(-3,5) == -8);
     }
 }
